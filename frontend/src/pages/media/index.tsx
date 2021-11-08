9f328a00e3a85c827afe417ab9816be533fcfb7e
Guilherme <ghpm99@hotmail.com>
2021-11-08 00:25:42 -0300

    Implement media table

--- frontend/src/pages/media/index.tsx
+++ frontend/src/pages/media/index.tsx
@@ -5,6 +5,38 @@ function Media() {
     return (
         <Container>
             <Title>Media</Title>
+            <div>Arquivos:</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Formato</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Arman Cekin - Run</td>
+                        <td>mp3</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>Playlist:</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Formato</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button>Enviar</button>
         </Container>
     )
 }
--- frontend/src/pages/media/styles.ts
+++ frontend/src/pages/media/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   background-color: #222831;
+  color: #EEEEEE;
 `;
 export const Title = styled.h1`
   color: #00adb5;
