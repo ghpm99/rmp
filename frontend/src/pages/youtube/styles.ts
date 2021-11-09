791044e3f9c86073bad56901508041468c85dc7b
Guilherme <ghpm99@hotmail.com>
2021-11-09 00:21:16 -0300

    Implements youtube page

--- frontend/src/pages/youtube/styles.ts
+++ frontend/src/pages/youtube/styles.ts
@@ -1,4 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  background-color: #222831;
+  color:#EEEEEE;
+`;
+
+export const Title = styled.h1`
+  color: #00adb5;
+  text-align: center;
+  margin: 0;
 `;
