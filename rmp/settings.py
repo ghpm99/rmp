71ac480dc1d36d7298f3d21cc3345691315c4719
Guilherme Henrique <ghpm99@hotmail.com>
2022-01-14 22:31:32 -0300

    Volta host name para settings

--- rmp/settings.py
+++ rmp/settings.py
@@ -91,7 +91,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD']
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST']
     }
 }
 
