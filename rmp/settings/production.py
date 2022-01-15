b1a9084d02a8edd1b6d814d96f11c5335597747a
Guilherme Henrique <ghpm99@hotmail.com>
2022-01-15 12:23:11 -0300

    Debuga ambiente de producao

--- rmp/settings/production.py
+++ rmp/settings/production.py
@@ -6,7 +6,7 @@ from rmp.settings.base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
