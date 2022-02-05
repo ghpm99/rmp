1707c068a299920661ea9490e47c98a17a7bb356
ghpm99 <ghpm99@hotmail.com>
2022-02-05 01:56:34 -0300

    Remove lib do gitignore

---
+++ lib/pusher/__init__.py
@@ -0,0 +1,14 @@
+import os
+import pusher
+
+PUSHER_CLIENT = pusher.Pusher(
+    app_id=os.environ.get('ENV_PUSHER_APP_ID'),
+    key=os.environ.get('ENV_PUSHER_KEY'),
+    secret=os.environ.get('ENV_PUSHER_SECRET'),
+    cluster=os.environ.get('ENV_PUSHER_CLUSTER'),
+    ssl=True
+)
+
+
+def send_command(command):
+    PUSHER_CLIENT.trigger('krill', 'command', {'cmd': command})
