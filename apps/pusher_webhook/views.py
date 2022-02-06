95ee49ef2aba6acc26e214cdd3cb78095fefbf53
ghpm99 <ghpm99@hotmail.com>
2022-02-05 22:48:40 -0300

    Ajusta webhook

--- apps/pusher_webhook/views.py
+++ apps/pusher_webhook/views.py
@@ -1,21 +1,12 @@
-from django.http import request
-from pusher import Pusher
 from django.views.decorators.csrf import csrf_exempt
 from rmp.decorators import add_cors_react_dev
 from django.views.decorators.http import require_POST
+from lib import pusher
 
 
 # Create your views here.
 @csrf_exempt
 @add_cors_react_dev
 @require_POST
-def pusher_webhook(request: request):
-    webhook = Pusher.validate_webhook(
-        key=request.headers.get('X-Pusher-Key'),
-        signature=request.headers.get('X-Pusher-Signature'),
-        body=request.data
-    )
-
-    print(webhook)
-
-    return 'ok'
+def pusher_webhook(request):
+    return pusher.webhook(request)
--- lib/pusher/__init__.py
+++ lib/pusher/__init__.py
@@ -1,5 +1,7 @@
 import os
+from django.http import JsonResponse
 import pusher
+import json
 
 PUSHER_CLIENT = pusher.Pusher(
     app_id=os.environ.get('ENV_PUSHER_APP_ID'),
@@ -12,3 +14,26 @@ PUSHER_CLIENT = pusher.Pusher(
 
 def send_command(command):
     PUSHER_CLIENT.trigger('krill', 'command', {'cmd': command})
+
+
+def webhook(request):
+    data = json.loads(request.body)
+    print(data)
+    webhook = PUSHER_CLIENT.validate_webhook(
+        key=request.headers.get('X-Pusher-Key'),
+        signature=request.headers.get('X-Pusher-Signature'),
+        body=request.body
+    )
+
+    if(webhook is None):
+        print('Webhook incorreto')
+        return JsonResponse({'msg': 'Webhook incorreto'}, status=400)
+
+    for event in webhook['events']:
+        if event['name'] == "channel_occupied":
+            print("Channel occupied: %s" % event["channel"])
+        elif event['name'] == "channel_vacated":
+            print("Channel vacated: %s" % event["channel"])
+    print(webhook)
+
+    return JsonResponse({'msg': 'ok'})
